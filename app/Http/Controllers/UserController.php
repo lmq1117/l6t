eb4ab67db7e5540ee6dbae104782b377b8b29b65
limq <limq@cgnfs.com.cn>
2019-10-29 17:39:36 +0800

    Debugbar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,34 +16,39 @@ class UserController extends Controller
         return $age->saySelfAge();
     }
 
-    public function redisSet($key,$value){
-        if(Redis::set($key,$value)){
-            return "set--- key:".$key."---value:".$value;
+    public function redisSet($key, $value)
+    {
+        if (Redis::set($key, $value)) {
+            return "set--- key:" . $key . "---value:" . $value;
         } else {
             return "啥也没干";
         }
     }
 
-    public function redisGet($key){
+    public function redisGet($key)
+    {
         return Redis::get($key);
     }
 
-    public function queue(){
-        for($i = 0;$i < 5000;$i++){
+    public function queue()
+    {
+        for ($i = 0; $i < 5000; $i++) {
             RandInsert::dispatch()->onConnection('l6t_queue')->onQueue('rand_insert');
         }
     }
 
-    public function pipeline(){
+    public function pipeline()
+    {
         return debug_backtrace();
     }
 
-    public function page(Request $request){
+    public function page(Request $request)
+    {
         //$articles = Article::simplePaginate($request->get("page",15));
         //$articles = Article::where('id','>','6000000')->limit(15)->get();
-        $page = $request->get('page',1);
-        $size = $request->get('size',15);
-        $articles = Article::whereRaw("id > (select max(id) from articles) - {$page}*")->orderBy('id','desc')->limit($size)->get();
+        $page = $request->get('page', 1);
+        $size = $request->get('size', 15);
+        $articles = Article::whereRaw("id > (select max(id) from articles) - " . $page * $size)->orderBy('id', 'desc')->limit($size)->get();
         return $articles;
     }
 
